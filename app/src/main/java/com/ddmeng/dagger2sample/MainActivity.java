2ea6edff8b5dac1c3f981b26f48409ae2c46d223
Dandan MENG <mengdandanno1@163.com>
2016-05-17 22:01:06 +0800

    show LogUtils now is not a Singleton

--- app/src/main/java/com/ddmeng/dagger2sample/MainActivity.java
+++ app/src/main/java/com/ddmeng/dagger2sample/MainActivity.java
@@ -1,7 +1,7 @@
 package com.ddmeng.dagger2sample;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 import com.ddmeng.dagger2sample.utils.LogUtils;
 
@@ -19,6 +19,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         ((SampleApplication) getApplication()).getComponent().inject(this);
-        logUtils.i("tag", "hi, I'm an instance of LogUtils");
+        logUtils.i(LogUtils.TAG, "hi, I'm an instance of LogUtils " + logUtils.hashCode());
+
+        ((SampleApplication) getApplication()).getComponent().inject(this);
+        logUtils.i(LogUtils.TAG, "hi, I'm another instance of LogUtils " + logUtils.hashCode());
     }
 }
--- app/src/main/java/com/ddmeng/dagger2sample/utils/LogUtils.java
+++ app/src/main/java/com/ddmeng/dagger2sample/utils/LogUtils.java
@@ -6,12 +6,15 @@ import javax.inject.Inject;
 
 public class LogUtils {
 
+    public static final String TAG = "Dagger2";
+
 
     // Use @Inject to annotate the constructor that Dagger should use to create instances of a class.
     // When a new instance is requested, Dagger will obtain the required parameters values and invoke this constructor.
 
     @Inject
     public LogUtils() {
+        Log.i(TAG, "constructor " + hashCode());
     }
 
     public void i(String tag, String info) {
